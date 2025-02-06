cafdd283c8bffd5593cea553750d678e88b5543e
Joaco <joa3quin12@gmail.com>
2025-02-06 01:34:03 +0100

    Update script.js

--- Pagina 2/script.js
+++ Pagina 2/script.js
@@ -25,7 +25,7 @@ const music_list = [
         img : 'images/tu.jpg',
         name : 'Tú',
         artist : 'maye',
-        music : 'https://www.youtube.com/watch?v=jhQ4WVJwz7w'
+        music : 'music/tu.mp3'
     },
     {
         img : 'images/bonita.jfif',
